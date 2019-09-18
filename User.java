bcd98e5f8c186be30792fb3f45d4bbc6c0f602de
blancpaix <54240763+blancpaix@users.noreply.github.com>
2019-09-18 20:20:01 +0900

    1.1
    1.1
    함수 추가 0918 20시19분

--- User.java
+++ User.java
@@ -1,5 +1,26 @@
 public class User {
 	String id;
+	String idRegex =  "\\w+@\\w+\\.(com|net|org|)";
 	String password;
+	String passwordRegex = "\\w{8,}";
 	
+	public boolean checkIdRegex(String input) {
+		if (!input.equals(idRegex)) {
+			System.out.println("입력한 ID가 형식과 일치하지 않습니다!");
+			return false;
+		} else {
+			System.out.println("ID 형식 확인완료!");
+			return true;
+		}
+	}
+	
+	public boolean checkPasswordRegex(String input) {
+		if (!input.equals(passwordRegex)) {
+			System.out.println("입력한 Password가 형식과 일치하지 않습니다!");
+			return false;
+		} else {
+			System.out.println("Password 형식 확인완료!");
+			return true;
+		}
+	}
 }
