8f3bbcc65d51fc1c5d13464dffb7144061a25b20
4transparency <53933566+4transparency@users.noreply.github.com>
2019-09-19 10:32:58 +0900

    v1.1

--- User.java
+++ User.java
@@ -1,21 +1,36 @@
 import java.util.regex.Pattern;
 
 public class User {
-	String id;
-	String password;
-	
+	private String id;
+	private String password;
+
 	String idregex = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
-	String pwRegex ="^[a-zA-Z0-9]{8,}$";
-	
+	String pwRegex = "^[a-zA-Z0-9]{8,}$";
+
 	public boolean valID(String str) {
-		boolean bl = str.matches(idregex);	
+		boolean bl = str.matches(idregex);
 		return bl;
 	}
+
 	public boolean valPassword(String str) {
-		boolean bl = str.matches(pwRegex);	
+		boolean bl = str.matches(pwRegex);
 		return bl;
 	}
-	
-	
-	
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 }
