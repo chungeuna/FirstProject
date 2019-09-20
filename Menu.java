9df79e18e2af27dc386f09661a737989a4fffc5c
blancpaix <54240763+blancpaix@users.noreply.github.com>
2019-09-20 12:10:18 +0900

    version. 1.1.1

--- Menu.java
+++ Menu.java
@@ -6,6 +6,30 @@ public class Menu {
 	Main main;
 	
 
+	Menu() {
+		boolean sholdExit = false;
+		while (true) {
+			System.out.println("─────────────────────────────");
+			System.out.println("1:회원가입     2:로그인     3:프로그램 종료");
+			System.out.println("─────────────────────────────");
+			int userInput = Integer.parseInt(sc.nextLine());
+			while (true) {
+				switch (userInput) {
+				case 1:	main.signIn(); // 회원가입 함수 호출
+					break;
+				case 2:	main.logIn(); // 로그인 함수 호출
+					break;
+				case 3:	System.out.println("프로그램 종료합니다");
+					System.exit(0);
+				default : 
+					System.out.println("잘못된 값을 입력하셨습니다.");
+				}
+			}
+		}
+	}
+	
+	
+	
 	int selectMainDisplay() {
 		System.out.println("─────────────────────────────");
 		System.out.println("1:회원가입     2:로그인     3:프로그램 종료");
@@ -82,6 +106,50 @@ public class Menu {
 				break;
 			default : 
 				System.out.println("잘못된 값을 입력하셨습니다.");
+			}
+		}
+	}
+	int selectEventManagingDisplay() {
+		System.out.println("───────────────────────────────────────────────────────────────");
+		System.out.println("1:전체 이벤트목록 출력 2:이벤트 입력 3:이벤트정보 변경 4.이벤트 삭제  0.뒤로가기");
+		System.out.println("───────────────────────────────────────────────────────────────");
+		int menu = 0;
+		do {
+			try {
+				menu = Integer.parseInt(sc.nextLine());
+				if (menu >= 0 && menu <=4) {
+					break;
+				} else {
+					throw new Exception("selectPersonManagerDisplay() Method 예외 발생");
+				}
+			} catch (Exception e) {
+				System.out.println("선택 문제");
+				System.out.println("0~4번까지 선택");
+			}
+		} while (true);		
+		return menu;		
+	}
+	
+	void showEventManagingDisplay() {
+		boolean shouldExit = false;
+		while (true) {
+			switch (selectEventManagingDisplay()) {
+			case 1:	// 1:전체 이벤트목록 출력
+				break;
+			case 2:	// 2:이벤트 입력
+				break;
+			case 3:	// 3:이벤트정보 변경
+				break;
+			case 4:	// 4.이벤트 삭제 
+				break;
+			case 0:	 //  0.뒤로가기
+				shouldExit = true;
+				break;
+			default : 
+				System.out.println("잘못된 값을 입력하셨습니다.");
+			}
+			if (shouldExit){
+				break;
 			}
 		}
 	}
